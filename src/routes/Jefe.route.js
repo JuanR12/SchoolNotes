14dee45e905e4fef12868ec2a95ad8f07dad7c89
JuanR12 <90404851+JuanR12@users.noreply.github.com>
2024-04-23 20:36:55 -0500

    Delete src directory

--- src/routes/Jefe.route.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-const AdministradorCtrl = require('../controllers/Jefe.Controllers')
-
-router.post('/crear', AdministradorCtrl.crearAdministrador)
-router.post('/login', AdministradorCtrl.login)
-module.exports = router
